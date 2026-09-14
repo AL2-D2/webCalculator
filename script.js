75104e0274d3272a1e60f4109e1d743de51906a5
AL2D2 <altug_cem2005@hotmail.com>
2026-09-15 01:46:22 +0300

    added mathematical functions to js. added some containers to the html.

---
+++ script.js
@@ -0,0 +1,5 @@
+const add = (firstNum,secondNum) => firstNum + secondNum;
+const subtract = (firstNum,secondNum) => firstNum - secondNum;
+const multiply = (firstNum,secondNum) => firstNum * secondNum;
+const divide = (firstNum,secondNum) => (firstNum / secondNum);
+
