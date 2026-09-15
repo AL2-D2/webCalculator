6e7f964a964b295255f672da9d6ebf0cdac9044d
AL2D2 <altug_cem2005@hotmail.com>
2026-09-15 19:59:33 +0300

    added operate function that checks what operator button is pressed and then make calculations based on this. also added event to subtract button and now it is functioning.

--- script.js
+++ script.js
@@ -1,41 +1,79 @@
-const add = (firstNum,secondNum) => firstNum + secondNum;
-const subtract = (firstNum,secondNum) => firstNum - secondNum;
-const multiply = (firstNum,secondNum) => firstNum * secondNum;
-const divide = (firstNum,secondNum) => firstNum / secondNum;
+const add = (firstNum, secondNum) => firstNum + secondNum;
+const subtract = (firstNum, secondNum) => firstNum - secondNum;
+const multiply = (firstNum, secondNum) => firstNum * secondNum;
+const divide = (firstNum, secondNum) => firstNum / secondNum;
 const decimal = (number) => Math.floor(number / 100);
 
+function operate(e, firstNumber, secondNumber) {
+    //checking what operator is pressed and make calculations based on this check.
+    if (e.target.classList.contains("add-btn")) {
+        return add(firstNumber, secondNumber);
+    }
+    else if (e.target.classList.contains("subtract-btn")) {
+        return subtract(firstNumber, secondNumber);
+    }
+    else if (e.target.classList.contains("multiply-btn")) {
+        return multiply(firstNumber, secondNumber);
+    }
+    else {
+        return divide(firstNumber, secondNumber);
+    }
+}
+
+function eventOperator(e) {
+    if (firstNumber != null && !resultScreenNumber.classList.contains("reset")) {
+        //must be converted to a number because operation functions take two number element as arguments.
+        secondNumber = Number(resultScreenNumber.innerText);
+        //sending e because we can check what operator is in operate function.        
+        result = operate(e, firstNumber, secondNumber);
+        resultScreenNumber.innerText = result;
+        //assigning firstNumber as the current result because it should continue to operate if another operation is desired from the user.
+        firstNumber = result;
+        //second number should be reset for future operations from the user.
+        secondNumber = null;
+        resultScreenNumber.classList.add("reset");
+    }
+    else {
+        /*stores the current element on the firstNumber and adds reset class because after the another 
+        numerical button is pushed it is going reset the currentScreenValue to the intended number.*/
+        firstNumber = Number(resultScreenNumber.innerText);
+        resultScreenNumber.classList.add("reset");
+    }
+}
+
 const resultScreenNumber = document.querySelector("#input-number");
-const numericalButtons  = document.querySelectorAll(".numerical-btn");
+const numericalButtons = document.querySelectorAll(".numerical-btn");
 const deleteButton = document.querySelector("#delete-btn");
 const clearButton = document.querySelector("#clear-btn");
 
 const addButton = document.querySelector("#add-btn");
+const subtractButton = document.querySelector("#subtract-btn");
 const resultButton = document.querySelector("#result-btn");
 //these numbers are stored for to be used later in the result button. they will be the arguments of the mathematical operation methods.
 let firstNumber = null;
 let secondNumber = null;
 let result = null;
 
-numericalButtons.forEach((numericalButton) => numericalButton.addEventListener("click", () => {    
+numericalButtons.forEach((numericalButton) => numericalButton.addEventListener("click", () => {
     // if result number has a 'reset' class make it disappear with the selected number...
-    if(resultScreenNumber.classList.contains("reset")){
+    if (resultScreenNumber.classList.contains("reset")) {
         resultScreenNumber.innerText = null;
         resultScreenNumber.classList.remove("reset");
     }
-    
+
     resultScreenNumber.innerText += numericalButton.innerText;
-    
+
 }));
 
 //delete the last digit on the number when clicked on the delete button.
 deleteButton.addEventListener("click", () => {
     //if its the only number on the screen make it 0 when clicked on the delete button.
-    if(resultScreenNumber.innerText.length === 1){
+    if (resultScreenNumber.innerText.length === 1) {
         resultScreenNumber.innerText = 0;
         resultScreenNumber.classList.add("reset");
     }
-    else{
-    resultScreenNumber.innerText = resultScreenNumber.innerText.substring(0,resultScreenNumber.innerText.length - 1);    
+    else {
+        resultScreenNumber.innerText = resultScreenNumber.innerText.substring(0, resultScreenNumber.innerText.length - 1);
     }
 });
 
@@ -45,26 +83,5 @@ clearButton.addEventListener("click", () => {
     resultScreenNumber.classList.add("reset");
 });
 
-
-addButton.addEventListener("click", () =>  {
-    /* if first number is a valid number and does not have reset class (which indicates that currently the operator button is not pressed which
-     prevents to assign current value to be second because it is already and must be the first value...) */
-    if(firstNumber != null && !resultScreenNumber.classList.contains("reset")){
-        //must be converted to a number because operation functions take two number element as arguments.
-        secondNumber = Number(resultScreenNumber.innerText);
-
-        result = add(firstNumber,secondNumber);
-        resultScreenNumber.innerText = result;
-        //assigning firstNumber as the current result because it should continue to operate if another operation is desired from the user.
-        firstNumber = result;
-        //second number should be reset for future operations from the user.
-        secondNumber = null;
-        resultScreenNumber.classList.add("reset");
-    }
-    else{
-    /*stores the current element on the firstNumber and adds reset class because after the another 
-    numerical button is pushed it is going reset the currentScreenValue to the intended number.*/
-    firstNumber = Number(resultScreenNumber.innerText);
-    resultScreenNumber.classList.add("reset");    
-    }
-});
+addButton.addEventListener("click", (e) => eventOperator(e));
+subtractButton.addEventListener("click", (e) => eventOperator(e));
