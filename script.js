ce59fd0395dfdacd1a21a2b60a22beea9c7d41f9
AL2D2 <altug_cem2005@hotmail.com>
2026-09-15 16:30:21 +0300

    added js event that only cumulatively shows the selected number button on the screen. only in numerical buttons rn.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const subtract = (firstNum,secondNum) => firstNum - secondNum;
 const multiply = (firstNum,secondNum) => firstNum * secondNum;
 const divide = (firstNum,secondNum) => (firstNum / secondNum);
 
+
+const resultScreenNumber = document.querySelector("#input-number");
+const numericalButtons  = document.querySelectorAll(".numerical-btn");
+
+
+numericalButtons.forEach((numericalButton) => numericalButton.addEventListener("click", (e) => {    
+    // if result number is 0 make it disappear with the selected number...
+    if(resultScreenNumber.innerText === "0"){
+        resultScreenNumber.innerText = null;
+    }
+    resultScreenNumber.innerText += numericalButton.innerText;
+}));
